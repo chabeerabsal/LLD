4149b9ffcd1a7e81971a5699bf08b413ec17efc0
chabeerabsal <chabeerabsal@gmail.com>
2025-09-06 11:48:14 +0530

    Elevator

--- Elevator/src/ElevatorCar.java
+++ Elevator/src/ElevatorCar.java
@@ -4,5 +4,47 @@ public class ElevatorCar {
     int currentfloor;
     ElevatorState elevatorState;
     ElevatorDisplay elevatorDisplay;
+    InternalButton internalButton;
 
+    ElevatorCar(){
+        elevatorDisplay=new ElevatorDisplay();
+        internalButton=new InternalButton();
+        elevatorState=ElevatorState.IDLE;
+        currentfloor=0;
+        direction=direction.UP;
+    }
+
+    public void showDisplay(){
+        elevatorDisplay.showDisplay();
+    }
+    public void setDisplay(){
+        elevatorDisplay.setDisplay(currentfloor,direction);
+    }
+    public void setInternalButton(int destination){
+        internalButton.pressButton(destination,this);
+    }
+    public boolean movecar(Direction direction,int destination){
+        int startfloor=currentfloor;
+
+        if(direction==Direction.UP){
+            for(int i=startfloor;i<=destination;i++){
+                if(i==destination){
+                    setDisplay();
+                    showDisplay();
+                    return true;
+                }
+            }
+        }
+        else if(direction==Direction.DOWN){
+            for(int i=startfloor;i>=destination;i--){
+                if(i==destination){
+                    setDisplay();
+                    showDisplay();
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
 }
--- Elevator/src/ElevatorController.java
+++ Elevator/src/ElevatorController.java
@@ -1,7 +1,13 @@
+import java.util.PriorityQueue;
+
 public class ElevatorController {
+    PriorityQueue<Integer> upMinQueue;
+    PriorityQueue<Integer> downMaxQueue;
     ElevatorCar elevatorCar;
     ElevatorController(ElevatorCar elevatorCar){
         this.elevatorCar = elevatorCar;
+        upMinQueue = new PriorityQueue<>();
+        downMaxQueue = new PriorityQueue<>((a,b)->b-a);
     }
 
     public void submitExternalRequest(int floorno,Direction direction){
