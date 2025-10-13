dd89f15114547954aa6656be24cf998425dd164d
chabeerabsal <chabeerabsal@gmail.com>
2025-10-13 21:51:53 +0530

    ATMMVP

--- ATM/ATM/src/STATE/IdleState.java
+++ ATM/ATM/src/STATE/IdleState.java
@@ -1,9 +1,25 @@
 package STATE;
 
-public abstract class IdleState implements ATMState{
+public class IdleState implements ATMState{
     @Override
     public void idleState(){
 
     }
 
+    @Override
+   public void HasCardState(){
+
+    }
+    @Override
+   public  void SelectOptionState(){
+
+    }
+    @Override
+    public void CashWithdrawalState(){
+
+    }
+    @Override
+    public void checkBalanceState(){
+        System.out.println("<UNK>");
+    }
 }
