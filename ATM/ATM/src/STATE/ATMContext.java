4aaf70a267972382ec9076f984c842b9716afa87
chabeerabsal <chabeerabsal@gmail.com>
2025-10-14 20:45:13 +0530

    ATMMVP

--- ATM/ATM/src/STATE/ATMContext.java
+++ ATM/ATM/src/STATE/ATMContext.java
@@ -5,6 +5,14 @@ public class ATMContext {
     ATMState atmState;
     ATMContext(){
         atmState=new IdleState();
-
     }
+
+    public ATMState getAtmState() {
+        return atmState;
+    }
+    public void setAtmState(ATMState atmState) {
+        this.atmState = atmState;
+    }
+
+
 }
