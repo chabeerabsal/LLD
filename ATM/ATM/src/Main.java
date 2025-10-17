1480fb9b828485a8755160c0cceba332c52124a1
chabeerabsal <chabeerabsal@gmail.com>
2025-10-17 22:59:03 +0530

    ATMMVP

--- ATM/ATM/src/Main.java
+++ ATM/ATM/src/Main.java
@@ -1,3 +1,6 @@
+import Modellor.ATMMachine;
+import STATE.ATMState;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,6 +8,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+
+        ATMMachine atmMachine = new ATMMachine();
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
