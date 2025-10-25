201c23ec97065ed17016699b9a21ce4b4d9cf2ec
chabeerabsal <chabeerabsal@gmail.com>
2025-10-25 23:32:55 +0530

    StatePattern

--- ATM/ATM/src/Main.java
+++ ATM/ATM/src/Main.java
@@ -1,12 +1,23 @@
 import Modellor.ATMMachine;
+import Modellor.BankAccount;
+import Modellor.Card;
+import Modellor.User;
 import STATE.ATMState;
+
+
+import javax.smartcardio.Card;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        Card card= new Card();
+        card.setNo(123);
+        card.setPin(1234);
+        BankAccount bankAccount = new BankAccount();
+        User user = new User(card,bankAccount);
+        user.getBankAccount().setBalance(1000);
+        A
 
     }
 }
