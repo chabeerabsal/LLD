7e2a2575a109c9d694047872dd4b212e954e7ec7
chabeerabsal <chabeerabsal@gmail.com>
2025-10-26 22:47:52 +0530

    ATMMVP

--- ATM/ATM/src/Modellor/ATM.java
+++ ATM/ATM/src/Modellor/ATM.java
@@ -1,9 +1,14 @@
 package Modellor;
 
+import STATE.ATMContext;
+import STATE.ATMState;
+import STATE.IdleState;
+
 public class ATM {
     ATMMachine atmMachine;
     UserController userController;
     //User user;
+    ATMContext atmContext;
 
     public ATM(ATMMachine atmMachine) {
         this.atmMachine = atmMachine;
@@ -12,7 +17,7 @@ public class ATM {
     public void verifyUser(User user) {
         if(userController.verifyUser(user))
         {
-
+           atmContext.setAtmState(new IdleState());
         }
         else{
 
